48ac4c30e8f9684f5512ef8668dde590269e53ef
kneekeel <nikhil.sodemba@gmail.com>
2020-11-09 15:38:27 -0500

    Single Child Scrollable added

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -272,115 +272,117 @@ class ViewMorePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("View More"),
       ),
-      body: SizedBox(
-        child: Card(
-          child: Column(
-            children: [
-              ListTile(
-                title: Text('Title',
-                    style:
-                        TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
-                subtitle: Text(hangout.title,
-                    style:
-                        TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
-                leading: Icon(
-                  Icons.title,
-                  color: Colors.orange[500],
+      body: SingleChildScrollView(
+        child: SizedBox(
+          child: Card(
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text('Title',
+                      style:
+                          TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
+                  subtitle: Text(hangout.title,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
+                  leading: Icon(
+                    Icons.title,
+                    color: Colors.orange[500],
+                  ),
                 ),
-              ),
-              Divider(),
-              ListTile(
-                title: Text('Start Time',
-                    style:
-                        TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
-                subtitle: Text(hangout.startTime,
-                    style:
-                        TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
-                leading: Icon(
-                  Icons.timelapse,
-                  color: Colors.orange[500],
+                Divider(),
+                ListTile(
+                  title: Text('Start Time',
+                      style:
+                          TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
+                  subtitle: Text(hangout.startTime,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
+                  leading: Icon(
+                    Icons.timelapse,
+                    color: Colors.orange[500],
+                  ),
                 ),
-              ),
-              Divider(),
-              ListTile(
-                title: Text('End Time',
-                    style:
-                        TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
-                subtitle: Text(hangout.endTime,
-                    style:
-                        TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
-                leading: Icon(
-                  Icons.timelapse,
-                  color: Colors.orange[500],
+                Divider(),
+                ListTile(
+                  title: Text('End Time',
+                      style:
+                          TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
+                  subtitle: Text(hangout.endTime,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
+                  leading: Icon(
+                    Icons.timelapse,
+                    color: Colors.orange[500],
+                  ),
                 ),
-              ),
-              Divider(),
-              ListTile(
-                title: Text('Event Type',
-                    style:
-                        TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
-                subtitle: Text(hangout.hangout_type,
-                    style:
-                        TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
-                leading: Icon(
-                  Icons.laptop_mac,
-                  color: Colors.orange[500],
+                Divider(),
+                ListTile(
+                  title: Text('Event Type',
+                      style:
+                          TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
+                  subtitle: Text(hangout.hangout_type,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
+                  leading: Icon(
+                    Icons.laptop_mac,
+                    color: Colors.orange[500],
+                  ),
                 ),
-              ),
-              Divider(),
-              ListTile(
-                title: Text('Category',
-                    style:
-                        TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
-                subtitle: Text(hangout.category,
-                    style:
-                        TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
-                leading: Icon(
-                  Icons.playlist_add_check,
-                  color: Colors.orange[500],
+                Divider(),
+                ListTile(
+                  title: Text('Category',
+                      style:
+                          TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
+                  subtitle: Text(hangout.category,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
+                  leading: Icon(
+                    Icons.playlist_add_check,
+                    color: Colors.orange[500],
+                  ),
                 ),
-              ),
-              Divider(),
-              ListTile(
-                title: Text('Location',
-                    style:
-                        TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
-                subtitle: Text(hangout.hangout_location,
-                    style:
-                        TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
-                leading: Icon(
-                  Icons.location_pin,
-                  color: Colors.orange[500],
+                Divider(),
+                ListTile(
+                  title: Text('Location',
+                      style:
+                          TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
+                  subtitle: Text(hangout.hangout_location,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
+                  leading: Icon(
+                    Icons.location_pin,
+                    color: Colors.orange[500],
+                  ),
                 ),
-              ),
-              Divider(),
-              ListTile(
-                title: Text('Contact',
-                    style:
-                        TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
-                subtitle: Text(hangout.contact,
-                    style:
-                        TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
-                leading: Icon(
-                  Icons.contact_phone,
-                  color: Colors.orange[500],
+                Divider(),
+                ListTile(
+                  title: Text('Contact',
+                      style:
+                          TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
+                  subtitle: Text(hangout.contact,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
+                  leading: Icon(
+                    Icons.contact_phone,
+                    color: Colors.orange[500],
+                  ),
                 ),
-              ),
-              Divider(),
-              ListTile(
-                title: Text('Description',
-                    style:
-                        TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
-                subtitle: Text(hangout.hangout_description,
-                    style:
-                        TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
-                leading: Icon(
-                  Icons.description,
-                  color: Colors.orange[500],
+                Divider(),
+                ListTile(
+                  title: Text('Description',
+                      style:
+                          TextStyle(fontSize: 13, fontWeight: FontWeight.w200)),
+                  subtitle: Text(hangout.hangout_description,
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.w500)),
+                  leading: Icon(
+                    Icons.description,
+                    color: Colors.orange[500],
+                  ),
                 ),
-              ),
-              Divider(),
-            ],
+                Divider(),
+              ],
+            ),
           ),
         ),
       ),
